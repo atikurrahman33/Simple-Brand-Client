04305a066ea3e9dff47332de1f1d94e7fbc67816
atikurrahman33 <foysalatik33@gmail.com>
2023-10-21 01:48:12 +0600

    add FAQ'S and team Mamber

--- src/Header and Footer/Team_member/Faq.jsx
+++ src/Header and Footer/Team_member/Faq.jsx
@@ -2,7 +2,52 @@
 const Faq = () => {
     return (
         <div>
-           
+            <div className='thumb'>
+            <h1 className="text-4xl font-bold text-center  mb-4">FAQ'S</h1>
+            </div>
+            <div className="faqs w-10/12 mx-auto my-12">
+                <div tabIndex={0} className="mt-4 collapse collapse-plus border border-base-300 bg-base-100 rounded">
+                    <div className="collapse-title font-semibold">
+                        How to Login in this website?
+                    </div>
+                    <div className="collapse-content">
+                        <p className='mt-6'>You can this website using google and email and password.</p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="mt-4 collapse collapse-plus border border-base-300 bg-base-100 rounded">
+                    <div className="collapse-title  font-semibold">
+                       What types of website is this?
+                    </div>
+                    <div className="collapse-content">
+                        <p>If you are user in this website you got some services. If you want to buy any tor you can do this and if you want sell any products toy realted you can buy here.</p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="mt-4 collapse collapse-plus border border-base-300 bg-base-100 rounded">
+                    <div className="collapse-title  font-semibold">
+                        How can I contact with your company?
+                    </div>
+                    <div className="collapse-content">
+                        <p>You can send message our contact section. Otherwise you can send email (ashraful.islam0871@gmail.com) here.</p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="mt-4 collapse collapse-plus border border-base-300 bg-base-100 rounded">
+                    <div className="collapse-title  font-semibold">
+                        Have any support?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Yes. We provide services 24/7 .</p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="mt-4 collapse collapse-plus border border-base-300 bg-base-100 rounded">
+                    <div className="collapse-title  font-semibold">
+                        Why I choose your company?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima doloribus qui consequuntur dolorum consequatur! Sit.</p>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 };
--- src/Header and Footer/Team_member/Team_member.jsx
+++ src/Header and Footer/Team_member/Team_member.jsx
@@ -2,7 +2,47 @@
 
 const Team_member = () => {
     return (
-       <div></div>
+        <div className="items-center justify-center bg-Black-50 mt-10 my-20">
+        <h2 className='text-4xl font-bold text-center'>Our Team Members</h2>
+        <div className="divider w-10/12 md:w-2/5 mx-auto mb-12">Meet our members</div>
+        <div className="w-10/12 gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto ">
+            <div data-aos="fade-up" className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+                <div className="h-96">
+                    <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src='https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVhbSUyMG1lbWJlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt="" />
+                </div>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+                <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                    <h1 className="font-dmserif text-3xl font-bold text-white">Marketing Manager</h1>
+                    <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 mt-12">Experienced Marketing Manager skilled in strategy development, team leadership, and driving business growth through innovative marketing campaigns.</p>
+
+                </div>
+            </div>
+            <div data-aos="fade-down" className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+                <div className="h-96">
+                    <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                </div>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+                <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                    <h1 className="font-dmserif text-3xl font-bold text-white">Product manager</h1>
+                    <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 mt-12">Results-driven Product Manager with expertise in product strategy, development, and launch. Passionate about delivering exceptional user experiences and driving business success.</p>
+
+                </div>
+            </div>
+            <div data-aos="fade-up" className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+                <div className="h-96">
+                    <img className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                </div>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+                <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                    <h1 className="font-dmserif text-3xl font-bold text-white mb-12">Software Engineering</h1>
+                    <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Innovative software engineer with a passion for problem-solving and creating efficient solutions. Experienced in full-stack development and proficient in various programming languages.</p>
+
+                </div>
+            </div>
+            
+        </div>
+
+    </div>
     );
 };
 
